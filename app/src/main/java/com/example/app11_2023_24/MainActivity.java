bde1ef0aeda2793793be4d9e216c2164dd69ac03
aibarra-vegga <aibarra-ext@veggadigital.com>
2023-11-29 16:19:01 +0100

    mig acabat

--- app/src/main/java/com/example/app11_2023_24/MainActivity.java
+++ app/src/main/java/com/example/app11_2023_24/MainActivity.java
@@ -1,44 +1,54 @@
 package com.example.app11_2023_24;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menuacciones, menu);
+        getMenuInflater().inflate(R.menu.menuacciones,menu);
         return true;
     }
 
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.icon_suma) {
             Intent intent = new Intent(this, Suma.class);
             intent.putExtra("suma", Suma.class.getName().toString());
             startActivity(intent);
-        }
-
-        if(id==R.id.icon_resta){
+            return true;
+        } else if (id == R.id.icon_resta) {
             Intent intent = new Intent(this, Resta.class);
             intent.putExtra("resta", Resta.class.getName().toString());
             startActivity(intent);
+            return true;
+        } else if (id == R.id.icon_multi) {
+            Intent intent = new Intent(this, Multiplicacio.class);
+            intent.putExtra("multi", Multiplicacio.class.getName().toString());
+            startActivity(intent);
+            return true;
+        } else if (id == R.id.icon_div) {
+            Intent intent = new Intent(this, Divisio.class);
+            intent.putExtra("div", Divisio.class.getName().toString());
+            startActivity(intent);
+            return true;
         }
-
         return super.onOptionsItemSelected(item);
-
     }
 
     public void suma(View view) {
@@ -64,5 +74,4 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra("div", Divisio.class.getName().toString());
         startActivity(intent);
     }
-
 }
